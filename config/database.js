de2d72fccc40bd2656fe61c679bbc0320a974b2c
shatroopa <shatroopa@gmail.com>
2025-12-10 06:12:40 +0300

    Clean backend for Railway deployment - Markaevi.com.tr

---
+++ config/database.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+  },
+});
---
+++ config/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS', ['key1', 'key2', 'key3', 'key4']),
+  },
+});
