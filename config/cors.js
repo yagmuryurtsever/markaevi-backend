057f12cb676fe6d3686e809344680572b9df368f
yagmuryurtsever <103523968+yagmuryurtsever@users.noreply.github.com>
2025-12-15 11:23:46 +0300

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,26 @@
 module.exports = ({ env }) => ({
   enabled: true,
-  origin: ['*'], // Production'da spesifik origin'ler belirleyin
+  origin: [
+    'https://markaevi.com.tr',
+    'https://www.markaevi.com.tr',
+    'http://localhost:3000',
+    'http://localhost:1337',
+    'https://api.markaevi.com.tr',
+    'http://api.markaevi.com.tr',
+    '*', // Tüm origin'lere izin ver (geçici çözüm)
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-  headers: '*',
+  headers: [
+    'Content-Type',
+    'Authorization',
+    'Origin',
+    'Accept',
+    'X-Requested-With',
+    'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Methods',
+    'Access-Control-Allow-Credentials'
+  ],
+  credentials: true,
   keepHeaderOnError: true,
 });
